1d3781a471aa54a331dd46469533aed7ee97a168
syamn <admin@sakura-server.net>
2012-12-29 07:24:44 +0900

    Add comments jdoc

--- Library/src/main/java/net/syamn/utils/ItemUtil.java
+++ Library/src/main/java/net/syamn/utils/ItemUtil.java
@@ -17,19 +17,19 @@ public class ItemUtil {
     private final static List<Integer> possibleRepairs;
     static{
         possibleRepairs = new LinkedList<Integer>();
-        for (int i = 256; i <= 259; i++){
+        for (int i = 256; i <= 259; i++){ // 3 Iron Tools, FlintSteel
             possibleRepairs.add(i);
         }
-        for (int i = 267; i <= 279; i++){
+        for (int i = 267; i <= 279; i++){ // Iron Sword, Wood Tools, Stone Tools, Diamond Tools
             possibleRepairs.add(i);
         }
-        for (int i = 283; i <= 286; i++){
+        for (int i = 283; i <= 286; i++){ // Gold Tools
             possibleRepairs.add(i);
         }
-        for (int i = 290; i <= 294; i++){
+        for (int i = 290; i <= 294; i++){ // Hoe
             possibleRepairs.add(i);
         }
-        for (int i = 298; i <= 317; i++){
+        for (int i = 298; i <= 317; i++){ // Armors
             possibleRepairs.add(i);
         }
         possibleRepairs.add(Material.SHEARS.getId());
@@ -38,6 +38,11 @@ public class ItemUtil {
         possibleRepairs.add(Material.CARROT_STICK.getId());
     }
     
+    /**
+     * アイテムIDが修理可能なものか返す
+     * @param id
+     * @return
+     */
     public boolean repairable(final int id){
         return possibleRepairs.contains(id);
     }
